194df6af594c5f958944136509ca58390f34b261
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2025-04-30 16:49:52 +0530

    attendance model is created

---
+++ kindergarten/src/main/java/com/nmpc/kindergarten/model/Attendance.java
@@ -0,0 +1,81 @@
+package com.nmpc.kindergarten.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "attendance")
+public class Attendance {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String playCenterId;
+
+	private LocalDate date;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	@JsonBackReference
+	private User user;
+
+	private boolean present;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public boolean isPresent() {
+		return present;
+	}
+
+	public void setPresent(boolean present) {
+		this.present = present;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getPlayCenterId() {
+		return playCenterId;
+	}
+
+	public void setPlayCenterId(String playCenterId) {
+		this.playCenterId = playCenterId;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Attendance [id=" + id + ", playCenterId=" + playCenterId + ", date=" + date + ", user=" + user + ", present=" + present + "]";
+	}
+
+}
